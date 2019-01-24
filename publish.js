fb34cc9a1a701aa38424fc9725b6c4f5eb3c77ac
Jefwillems <jefwillems100@gmail.com>
2019-01-24 12:17:02 +0100

    Change all colors to white and #333

--- publish.js
+++
@@ -1,9 +0,0 @@
-const github_pages = require('gh-pages');
-
-github_pages.publish('.', { dotfiles: true }, function(err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Push succesfull');
-  }
-});
